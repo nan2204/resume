99fdd2c8ec818efdfbd355df41c2ac0a711d13ec
nan2204 <727721euec096@skcet.ac.in>
2023-04-20 21:40:07 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+export default class Footer extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">
+            {
+              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
+                return(
+                  <li>
+                    <h2>CONTACT ME</h2>
+                    <h2>mob num: 9345833432</h2>
+                    <a href={item.url}>
+                    <i className={item.className} />
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+      </div>
+    </footer>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,68 @@
+let resumeData = {
+   
+    "name": "NANDANA P",
+    "role": "Student at Sri Krishna college of Engineering and Technology",
+  
+    "roleDescription": "A Student who like to learn new technologies in this emerging world and create something new that would change this world",
+    "socialLinks":[
+       
+        {
+          "name":"github",
+          "url":"https://github.com/nan2204",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    
+   
+    "education":[
+      {
+        "UniversityName":"Sri Krishna College of Engineering and Technology",
+        "specialization":"B.E ECE",
+       
+        "Achievements":" Participated in paper presentation - NEXUS_2K22 , Participated in Sensation_23 "
+      },
+      {
+        "UniversityName":"Anna University",
+        "specialization":"Electronics and Communication Engineering",
+        
+      }
+    ],
+    
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C , C++ ,JAVA"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        
+        "skillname":"DBMS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"travelogue",
+       
+      },
+     
+    ],
+    "testimonials":[
+      {
+        "description":"THINK POSITIVE , AND WORK TOWARDS YOUR GOALS",
+         
+      },
+     
+    ]
+  }
+  
+  export default resumeData
